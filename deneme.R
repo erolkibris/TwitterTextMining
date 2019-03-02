d393692e584559999a6613cbe2060c010c274a76
Erol Kibris <erolkibris@gmail.com>
2019-03-02 13:31:35 +0200

    migren kelimesi geçen tweetler

--- deneme.R
+++ deneme.R
@@ -7,10 +7,6 @@ remotes::install_github("mkearney/rtweet")
 
 
 library(rtweet)
-library(tm)
-library(SnowballC)
-library(wordcloud)
-library(RColorBrewer)
 
 migo <- search_tweets(
   q="migren", n=18000 ,retryonratelimit = TRUE, include_rts = FALSE
