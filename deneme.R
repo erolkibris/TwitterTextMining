6036732f80ee2ac880792cb1b1c737053713807e
Erol Kibris <erolkibris@gmail.com>
2019-03-03 13:15:26 +0200

    dataframe düzenlendi

--- deneme.R
+++ deneme.R
@@ -6,11 +6,25 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 remotes::install_github("mkearney/rtweet")
 
 
+
 library(rtweet)
+library(dplyr)
+library(lubridate)
 
 migo <- search_tweets(
   q="migren", n=18000 ,retryonratelimit = TRUE, include_rts = FALSE
 )
 
+migren_df <- as.data.frame(migo)
 
+migren_tr <- migren_df %>%
+  select(status_id, created_at, text, location)%>%
+  filter(migren_df$lang =="tr")%>%
+  arrange(migren_tr$created_at)
 
+migren_tr$date <- as.Date(migren_tr$created_at)
+migren_tr$time <- format(migren_tr$created_at,"%H:%M:%S")
+
+migren_tr$day <- wday(migren_tr$date,label = TRUE)
+
+  
