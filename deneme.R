23f46bf229329e8a42fdea768406a232213cc3e0
Erol Kibris <erolkibris@gmail.com>
2019-03-06 11:47:54 +0200

    freq oluşturuldu.

--- deneme.R
+++ deneme.R
@@ -10,6 +10,9 @@ remotes::install_github("mkearney/rtweet")
 library(rtweet)
 library(dplyr)
 library(lubridate)
+library(chron)
+library(plyr)
+library(shiny)
 
 migo <- search_tweets(
   q="migren", n=18000 ,retryonratelimit = TRUE, include_rts = FALSE
@@ -22,10 +25,15 @@ migren_tr <- migren_df %>%
   filter(migren_df$lang =="tr")%>%
   arrange(migren_tr$created_at)
 
+migren_tr$created_at <- round_date(migren_tr$created_at, "hour", week_start = getOption("lubridate.week.start",7))
+
 migren_tr$date <- as.Date(migren_tr$created_at)
-migren_tr$time <- format(migren_tr$created_at,"%H:%M:%S")
+migren_tr$time <- format(migren_tr$created_at, "%H")
 
 migren_tr$day <- wday(migren_tr$date,label = TRUE)
 
-save(migren_tr, file = "migren_tr.RData")
-  
+day_freq <- count(migren_tr,'day')
+time_freq <- count(migren_tr, 'time')
+date_freq <- count(migren_tr, 'date')
+
+
