3917c265d9894ec8209e0b6431d552de3a7db497
Erol Kibris <erolkibris@gmail.com>
2019-03-11 12:38:59 +0200

    yeni veriler çekildi

--- deneme.R
+++ deneme.R
@@ -23,16 +23,19 @@ migo <- search_tweets(
 
 migren_df <- as.data.frame(migo)
 
-migren_tr <- migren_df %>%
+migren_triki <- migren_df %>%
   select(status_id, created_at, text, location)%>%
-  filter(migren_df$lang =="tr")%>%
-  arrange(migren_tr$created_at)
+  filter(migren_df$lang =="tr")
+  
+  
 
-migren_tr$created_at <- round_date(migren_tr$created_at, "hour", week_start = getOption("lubridate.week.start",7))
+migren_triki$created_at <- round_date(migren_triki$created_at, "hour", week_start = getOption("lubridate.week.start",7))
 
-migren_tr$date <- as.Date(migren_tr$created_at)
-migren_tr$time <- format(migren_tr$created_at, "%H")
-migren_tr$day <- wday(migren_tr$date,label = TRUE)
+migren_triki$date <- as.Date(migren_triki$created_at)
+migren_triki$time <- format(migren_triki$created_at, "%H")
+migren_triki$day <- wday(migren_triki$date,label = TRUE)
+
+
 
 day_freq <- count(migren_tr,'day')
 time_freq <- count(migren_tr, 'time')
@@ -46,15 +49,17 @@ bur <- sum(migren_tr$location %s/% 'Bursa')
 cities <- data.frame("Cities" = c("İstanbul","Ankara", "İzmir", "Bursa", "Diğer"), 
                      "Frequency" = c(ist, ank, izm, bur, 1117 - (ist+ank+izm+bur)))
 
-cities$perc <- cities$Frequency / sum(cities$Frequency)*100
+cities$Perc <- cities$Frequency / sum(cities$Frequency)*100
+
 
 ggplot(data = cities)+
-  aes(x=Cities, y = perc)+
-  geom_bar(stat="identity")+
+  aes(x=Cities, y = Perc)+
+  geom_bar(stat="identity",fill="light blue")+
   theme(panel.background = element_rect(fill = "white"))+
   labs(title = "Şehirlere göre atılan tweetler")+
   xlab("Şehirler")+
   ylab("Yüzdeler")
+  
 
 
   
