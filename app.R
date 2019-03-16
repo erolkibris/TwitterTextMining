c99cf2ff72832a494a7fc209e13849f58b1ad50c
Erol Kibris <erolkibris@gmail.com>
2019-03-16 12:58:53 +0200

    shiny app ve yeni düzenlemeler

---
+++ app.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(ggplot2)
+library(reshape2)
+
+ui <- fluidPage(
+  title = "Gün-Tweet Sayısı",
+  titlePanel("Gün ve Saate Göre Atılan Tweet Sayısı"),
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("gun", 
+                         h1("Gunu seciniz."), 
+                         choices = list("Pazartesi" = "Pzt", "Sali" = "Sal",
+                                        "Carsamba" = "Car", "Persembe" = "Per",
+                                        "Cuma" = "Cum", "Cumartesi" = "Cmt", 
+                                        "Pazar" ="Paz", "Ortalama" = "Ort",
+                                        "Hafta ici" = "hici", "Hafta sonu" = "hsonu"))
+    ),
+    mainPanel(
+      plotOutput("plot"))
+  )
+)
+
+
+server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    plot.data <- melt(x, id.vars = 'saat')
+    plot.data <- plot.data[plot.data$variable %in% input$gun, ]
+    ggplot(data=plot.data)+
+      geom_line(mapping = aes(x=saat, y= value, colour = variable))+
+      theme_classic()+
+      scale_x_continuous(breaks = c(0:23))+
+      ylab("Tweetler")+
+      xlab("Saat")
+    
+      
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
--- deneme.R
+++ deneme.R
@@ -16,6 +16,7 @@ library(roperators)
 library(ggplot2)
 library(tidyr)
 library(scales)
+library(reshape2)
 
 migo <- search_tweets(
   q="migren", n=18000 ,retryonratelimit = TRUE, include_rts = FALSE
@@ -25,7 +26,9 @@ migren_df <- as.data.frame(migo)
 
 migren_triki <- migren_df %>%
   select(status_id, created_at, text, location)%>%
-  filter(migren_df$lang =="tr")
+  filter(migren_df$lang =="tr")%>%
+  arrange(migren_triki$created_at)
+  
   
   
 
@@ -35,16 +38,26 @@ migren_triki$date <- as.Date(migren_triki$created_at)
 migren_triki$time <- format(migren_triki$created_at, "%H")
 migren_triki$day <- wday(migren_triki$date,label = TRUE)
 
+migren <- rbind(migren_tr, migren_triki[124:1040,])
 
+migren_time = data.frame(migren$day, migren$time)
 
-day_freq <- count(migren_tr,'day')
-time_freq <- count(migren_tr, 'time')
-date_freq <- count(migren_tr, 'date')
+migren_time %>%
+  group_by(migren.day, migren.time)
+ 
+sum_table <- as.data.frame(table(migren_time$migren.day,migren_time$migren.time))
+  
+sum_table <- sum_table %>% 
+  arrange(Var1)
 
-ist <- sum(migren_tr$location %s/% 'İstanbul') 
-ank <- sum(migren_tr$location %s/% 'Ankara')
-izm <- sum(migren_tr$location %s/% 'İzmir')
-bur <- sum(migren_tr$location %s/% 'Bursa')
+day_freq <- count(migren,'day')
+time_freq <- count(migren, 'time')
+date_freq <- count(migren, 'date')
+
+ist <- sum(migren$location %s/% 'İstanbul') 
+ank <- sum(migren$location %s/% 'Ankara')
+izm <- sum(migren$location %s/% 'İzmir')
+bur <- sum(migren$location %s/% 'Bursa')
 
 cities <- data.frame("Cities" = c("İstanbul","Ankara", "İzmir", "Bursa", "Diğer"), 
                      "Frequency" = c(ist, ank, izm, bur, 1117 - (ist+ank+izm+bur)))
@@ -59,6 +72,31 @@ ggplot(data = cities)+
   labs(title = "Şehirlere göre atılan tweetler")+
   xlab("Şehirler")+
   ylab("Yüzdeler")
+
+
+sun_data = sum_table[1:24,]
+ggplot(data=sun_data, aes(x=Var2, y= Freq, group =1))+
+  geom_line()+
+  geom_line(aes(y=time_day$Mean, col ="red"))
+
+
+
+x <- data.frame(matrix(,nrow = 24, ncol = 9))
+coln <- c("saat", "Pzt", "Sal", "Car", "Per", "Cum", "Cmt", "Paz", "Ort")
+colnames(x) <- coln
+x$saat <- c(0:23)
+x$Ort <- time_day$Mean
+x[,(c("Paz", "Sal", "Car", "Per", "Cum", "Cmt", "Pzt"))]=t(ldply(split(sum_table$Freq, sum_table$Var1))[,-1])
+
+x$hici <- with(x[,2:6], rowSums(x[,2:6]))
+x$hsonu <- with(x[,7:8], rowSums(x[,7:8]))
+
+
+
+
+  
+  
+ 
   
 
 
